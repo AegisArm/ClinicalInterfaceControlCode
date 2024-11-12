17f15646032b0b1f5a97c1a5047f71226345424d
Dee <dromero@bancroftschool.org>
2024-11-12 09:13:40 -0500

    loads aegis specific arm

--- main.groovy
+++ main.groovy
@@ -1,1 +1,14 @@
 println("starting");
+	base=DeviceManager.getSpecificDevice( "Standard6dof",{
+			//If the device does not exist, prompt for the connection
+			
+			MobileBase m = MobileBaseLoader.fromGit(
+				"https://github.com/AegisArm/GroguMechanicsCad.git",
+				"hephaestus.xml"
+				)
+			if(m==null)
+				throw new RuntimeException("Arm failed to assemble itself")
+			println "Connecting new device robot arm "+m
+			return m
+		})
+		return null;
