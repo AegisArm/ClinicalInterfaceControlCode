5c6afbb50433e5269852e6c723d4db11a16deed4
Dee <dromero@bancroftschool.org>
2024-11-12 09:38:43 -0500

    transforming in increments

--- main.groovy
+++ main.groovy
@@ -13,9 +13,24 @@ MobileBase base=DeviceManager.getSpecificDevice( "Standard6dof",{
 })
 
 DHParameterKinematics aegisArm = base.getAllDHChains().get(0);
+double zLift = 20;
+
+TransformNR offset = new TransformNR(0, 0, zLift);
+
+
+//Start from where the arm already is and move it from there with absolute location
+TransformNR current = aegisArm.calcHome().copy();
+//current.translateZ(zLift);
 
 
 
+for(double i = 0; i <= 1; i += 0.01){
+	TransformNR incre = offset.scale(i);
+	TransformNR temp = incre.times(current);//multiply offset by current
+	aegisArm.setDesiredTaskSpaceTransform(temp,  0);//change this value to smooth out motion
+	Thread.sleep(30); //wait 30 msec
+}
+// size of increment, and wait determine velocity of motion
 
 
 
